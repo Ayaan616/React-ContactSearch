d0bb338629cf71d30218493e5973492664379778
Ayaan616 <89765470+Ayaan616@users.noreply.github.com>
2022-09-23 19:58:42 +0530

    Update card.components.jsx
    Incremented by 3 to render the not showing images down by jsonplaceholder.io

--- src/components/card/card.components.jsx
+++ src/components/card/card.components.jsx
@@ -4,7 +4,7 @@ import "./card.styles.css";
 const Card = (props) => {
     return (    
         <div className='card-container'>
-        <img alt='any' src={`https://robohash.org/${props.monster.id}?set=set5&size=180x180`}></img>
+        <img alt='any' src={`https://robohash.org/${props.monster.id+3}?set=set5&size=180x180`}></img>
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
     </div>
